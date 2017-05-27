6383a94c62710062acd74b8697474c191c7bca0a
Luis Alberto Pereyra <pereyra.luis.alb@gmail.com>
2017-05-26 21:49:20 -0300

    Me olvide de declarar el arreglo *Id

--- peano.c
+++ peano.c
@@ -10,6 +10,7 @@
 void peano_hilbert()
 {
   int i;
+  int *Id;
   struct peano_hilbert_data *mp;
   peanokey MinTot = (((peanokey)1)<<(3*BITS_PER_DIMENSION));
   type_real DomainFac = 1.0 / (cp.lbox*1.001) * (((peanokey) 1) << (BITS_PER_DIMENSION));
